fa299c73f36c7977ec8a33d51910218351c955c4
Azamat <azamatsatynbekov@gmail.com>
2020-03-14 13:37:18 +0600

    Этап 2 сделан, Вывел на странице пользователя список файлов, c сортировкой по дате и спагинацией

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,6 +7,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView
 from accounts.forms import SignUpForm
+from webapp.models import File
 
 
 class SignUp(CreateView):
@@ -18,3 +20,19 @@ class UserDetailView(DetailView):
     model = User
     template_name = 'user_detail.html'
     context_object_name = 'user_obj'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        files = context['user_obj'].users_file.order_by('-created_date')
+        self.paginate_files_to_context(files, context)
+        # context['files'] = files
+        return context
+
+    def paginate_files_to_context(self, files, context):
+        paginator = Paginator(files, 10, 0)
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        context['paginator'] = paginator
+        context['page_obj'] = page
+        context['files'] = page.object_list
+        context['is_paginated'] = page.has_other_pages()
